216a75a4579326b6ebd831f99f31f2a1aec4ab74
Kyle Coberly <kyle.coberly@gmail.com>
2022-10-08 21:32:28 -0600

    Add solution

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import classNames from 'classnames';
+import {useState} from 'react';
 
 function App() {
+  const [lightOn, setLightOn] = useState(false)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button
+          onClick={() => setLightOn(!lightOn)}
+          className={classNames({
+            switch: true,
+            on: !lightOn,
+            off: lightOn,
+          })}
+        >{ lightOn ? "Turn Off" : "Turn On"}</button>
+        <div
+          className={classNames({
+            lightbulb: true,
+            on: lightOn,
+            off: !lightOn,
+          })}
+        >&nbsp;</div>
+      </div>
     </div>
   );
 }
